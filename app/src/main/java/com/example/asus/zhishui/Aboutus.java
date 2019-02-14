2919cbaa19d8d4990b482f1692ca951d2d7470fe
vivificationLeslie <vivificationleslie@gmail.com>
2019-02-14 19:34:05 +0800

    layout change

--- app/src/main/java/com/example/asus/zhishui/Aboutus.java
+++ app/src/main/java/com/example/asus/zhishui/Aboutus.java
@@ -1,20 +1,40 @@
 package com.example.asus.zhishui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
 public class Aboutus extends AppCompatActivity {
 
+    Button renew;
+    Button feedback;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
         initView();
+
+        feedback.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:1511432@mail.nankai.edu.cn"));
+                intent.putExtra(Intent.EXTRA_SUBJECT,"止水APP反馈");
+                intent.putExtra(Intent.EXTRA_TEXT,"请填写反馈问题/建议");
+                startActivity(intent);
+            }
+        }
+
+        ));
 
     }
 
