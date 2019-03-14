d71ce8793c14a0222195b0de64e2a9afb0e988fc
vivificationLeslie <vivificationleslie@gmail.com>
2019-03-14 19:18:03 +0800

    变更 1.所有界面优化 2.添加情绪易感性测量 3.bug修改 4.audio还没有放进去

--- app/src/main/java/com/example/asus/zhishui/bookpage/Dataset.java
+++ app/src/main/java/com/example/asus/zhishui/bookpage/Dataset.java
@@ -13,16 +13,15 @@ import com.example.asus.zhishui.R;
 public class Dataset {
 
     private List<ECCardData> dataset;
-
     public Dataset() {
         dataset = new ArrayList<>(5);
 
         CardData item5 = new CardData();
         item5.setMainBackgroundResource(R.drawable.attractions);
         item5.setHeadBackgroundResource(R.drawable.attractions_head);
-        item5.setHeadTitle("Calm");
-        item5.setPersonMessage("Usus de bassus buxum, desiderium index!");
-        item5.setPersonName("Gao Qi");
+        item5.setHeadTitle("Relaxed");
+        item5.setPersonMessage("If you do what you love, it is the best way to relax.");
+        item5.setPersonName("Christian Louboutin");
         item5.setListItems(prepareContentsArray5());
         dataset.add(item5);
 
@@ -30,17 +29,17 @@ public class Dataset {
         item4.setMainBackgroundResource(R.drawable.city_scape);
         item4.setHeadBackgroundResource(R.drawable.city_scape_head);
         item4.setHeadTitle("Angry");
-        item4.setPersonMessage("Solems manducare, tanquam neuter verpa.");
-        item4.setPersonName("Huang Yan");
+        item4.setPersonMessage("Negative emotions are big, flashing signs that something needs to change.");
+        item4.setPersonName("Gretchen Rubin");
         item4.setListItems(prepareContentsArray4());
         dataset.add(item4);
 
         CardData item3 = new CardData();
         item3.setMainBackgroundResource(R.drawable.cuisine);
         item3.setHeadBackgroundResource(R.drawable.cuisine_head);
-        item3.setHeadTitle("Upset");
-        item3.setPersonMessage("Magnum lacteas ducunt ad orexis.");
-        item3.setPersonName("Li Hao");
+        item3.setHeadTitle("Depressed");
+        item3.setPersonMessage("You can choose to not let little things upset you.");
+        item3.setPersonName("Joel Osteen");
         item3.setListItems(prepareContentsArray3());
         dataset.add(item3);
 
@@ -48,17 +47,17 @@ public class Dataset {
         item2.setMainBackgroundResource(R.drawable.nature);
         item2.setHeadBackgroundResource(R.drawable.nature_head);
         item2.setHeadTitle("Anxious");
-        item2.setPersonName("Si Keyi");
-        item2.setPersonMessage("Nunquam perdere clabulare.");
+        item2.setPersonName("Olive Schreiner");
+        item2.setPersonMessage("No good work is ever done while the heart is hot and anxious and fretted.");
         item2.setListItems(prepareContentsArray2());
         dataset.add(item2);
 
         CardData item1 = new CardData();
         item1.setMainBackgroundResource(R.drawable.night_life);
         item1.setHeadBackgroundResource(R.drawable.night_life_head);
-        item1.setHeadTitle("Sad");
-        item1.setPersonMessage("Cur adelphis studere?");
-        item1.setPersonName("Yu Renshu");
+        item1.setHeadTitle("Panic");
+        item1.setPersonMessage("Stay in control, and never panic.");
+        item1.setPersonName("Martha Stewart");
         item1.setListItems(prepareContentsArray1());
         dataset.add(item1);
 
@@ -74,9 +73,9 @@ public class Dataset {
     private List<Content> prepareContentsArray1() {
         List<Content> contents = new ArrayList<>();
         contents.addAll(Arrays.asList(
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1)));
+                new Content("What is a panic attack?", R.string.panic1),
+                new Content("When do panic attacks happen?", R.string.panic2),
+                new Content("What is innovation?", R.string.panic3)));
         return contents.subList(0, 3);
 
     }
@@ -84,9 +83,9 @@ public class Dataset {
     private List<Content> prepareContentsArray2() {
         List<Content> contents = new ArrayList<>();
         contents.addAll(Arrays.asList(
-                new Content("Why is innovation?", R.string.content1),
-                new Content("Why is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1)));
+                new Content("How can I deal with pressure?", R.string.stress1),
+                new Content("How can I become more emotionally resilient?", R.string.stress2),
+                new Content("What treatments are there for stress?", R.string.stress3)));
         return contents.subList(0, 3);
 
     }
@@ -94,9 +93,11 @@ public class Dataset {
     private List<Content> prepareContentsArray3() {
         List<Content> contents = new ArrayList<>();
         contents.addAll(Arrays.asList(
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1)));
+                new Content("Look after yourself", R.string.depression1),
+                new Content("Practice self-care", R.string.depression2),
+                new Content("Keep active", R.string.depression3),
+                new Content("Challenge your low mood", R.string.depression4),
+                new Content("Connect with other people", R.string.depression5)));
         return contents.subList(0, 3);
 
     }
@@ -104,9 +105,9 @@ public class Dataset {
     private List<Content> prepareContentsArray4() {
         List<Content> contents = new ArrayList<>();
         contents.addAll(Arrays.asList(
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1)));
+                new Content("What is unhelpful angry behaviour?", R.string.angry1),
+                new Content("How can I manage an outburst?", R.string.angry2),
+                new Content("How can I control my anger long term?", R.string.angry3)));
         return contents.subList(0, 3);
 
     }
@@ -114,9 +115,9 @@ public class Dataset {
     private List<Content> prepareContentsArray5() {
         List<Content> contents = new ArrayList<>();
         contents.addAll(Arrays.asList(
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1),
-                new Content("What is innovation?", R.string.content1)));
+                new Content("Relax your body", R.string.relax1),
+                new Content("Get creative", R.string.relax2),
+                new Content("Listen music", R.string.relax3)));
         return contents.subList(0, 3);
 
     }
